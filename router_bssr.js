e3510a59185491e420ba960d704d46b79ad3bfae
ShakhriyorNorkobilovich <shaxroyorbek2101@gmail.com>
2023-11-06 22:38:36 +0900

    feat: develop admin related restaurant control page

--- router_bssr.js
+++ router_bssr.js
@@ -35,5 +35,9 @@ router_bssr.post("/products/edit/:id",
 restaurantController.validateAuthRestaurant, 
 productController.updateChosenProduct);
 
+router_bssr.get("/all-restaurant", 
+restaurantController.validateAdmin, 
+restaurantController.getAllRestaurants);
+
 
 module.exports = router_bssr;
