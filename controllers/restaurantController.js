f9edceead0dbb767973fe8599e7af951a4e87583
ShakhriyorNorkobilovich <shaxroyorbek2101@gmail.com>
2023-12-06 18:45:35 +0900

    feat: dvelop getRestaurants api business logic

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -3,14 +3,38 @@ const Member = require("../models/Member");
 const Product = require("../models/Product");
 const assert = require("assert");
 const Restaurant = require("../models/Restaurant");
+const { response } = require("express");
 
 let restaurantController = module.exports;
+
+restaurantController.getRestaurants = async (req, res) =>{
+    try{
+        console.log("GET: cont/getRestaurants");
+        const data = req.query,
+            restaurant = new Restaurant(),
+            result = await restaurant.getRestaurantsData(req.member, data);
+        res.json({state: "success", data: result});
+
+
+    }   catch(err) {
+        console.log(`ERROR, cont/home, ${err.message}`);
+        res.json({state: 'fail', message: err.message});
+    }
+}
+
+
+
+
+
+/******************************************************
+ *            BSSR RELATED METHODS                    *
+ ******************************************************/
 
 restaurantController.home = (req, res) => {
     try {
         console.log("GET: cont/home");
         res.render('home-page');
-    }catch{
+    }catch (err) {
         console.log(`ERROR, cont/home, ${err.message}`);
         res.json({state: 'fail', message: err.message});
     }
@@ -124,7 +148,7 @@ restaurantController.validateAuthRestaurant = (req, res, next) => {
 
 restaurantController.checkSessions = (req, res) => {
     if(req.session?.member){
-        res.json({state: 'succeed', data: req.session.member})
+        res.json({state: 'success', data: req.session.member})
     } else{
         res.json({state: 'fail', message: "You are not authenticated"});
     }
