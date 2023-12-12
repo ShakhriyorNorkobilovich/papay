9a4b0babcce9a4cc19ad3e07046b6994f8df1a89
ShakhriyorNorkobilovich <shaxroyorbek2101@gmail.com>
2023-12-12 16:39:27 +0900

    feat: develop getMemberArticles api business logic

---
+++ controllers/communityController.js
@@ -0,0 +1,52 @@
+let communityController = module.exports;
+const assert = require("assert");
+const Definer = require("../lib/mistake");
+const Community = require("../models/Community");
+
+communityController.imageInsertion = async (req, res) => {
+  try {
+    console.log("POST: cont/imageInsertion");
+    assert.ok(req.file, Definer.general_err3);
+    const image_url = req.file.path;
+    res.json({ state: "success", data: image_url });
+  } catch (err) {
+    console.log(`ERROR, cont/imageInsertion, ${err.message} `);
+    res.json({ state: "fail", message: err.message });
+  }
+};
+
+communityController.createArticle = async (req, res) => {
+  try {
+    console.log("POST: cont/createArticle");
+    const community = new Community();
+    const result = await community.createArticleData(req.member, req.body);
+
+    assert.ok(result, Definer.general_err3);
+
+    res.json({ state: "success", data: result });
+  } catch (err) {
+    console.log(`ERROR, cont/createArticle, ${err.message} `);
+    res.json({ state: "fail", message: err.message });
+  }
+};
+
+communityController.getMemberArticles = async (req, res) => {
+  try {
+    console.log("GET: cont/getMemberArticles");
+    const community = new Community();
+    
+    const mb_id = req.query.mb_id !== "none" ? req.query.mb_id : req.member?._id;
+    assert.ok(mb_id, Definer.article_err1);
+    
+    const result = await community.getMemberArticlesData(
+      req.member,
+      mb_id,
+      req.query
+    );
+
+    res.json({ state: "success", data: result });
+  } catch (err) {
+    console.log(`ERROR, cont/getMemberArticles, ${err.message} `);
+    res.json({ state: "fail", message: err.message });
+  }
+};
---
+++ models/Community.js
@@ -0,0 +1,61 @@
+const BoArticleModel = require("../schema/bo_article");
+const Definer = require("../lib/mistake");
+const assert = require("assert");
+const { shapeIntoMongooseObjectId } = require("../lib/config");
+
+class Community {
+  constructor() {
+    this.boArticleModel = BoArticleModel;
+  }
+
+  async createArticleData(member, data) {
+    try {
+      data.mb_id = shapeIntoMongooseObjectId(member._id);
+      const new_article = await this.saveArticleData(data);
+      return new_article;
+    } catch (err) {
+      throw err;
+    }
+  }
+  async saveArticleData(data) {
+    try {
+      const article = new this.boArticleModel(data);
+      return await article.save();
+    } catch (mongo_err) {
+      console.log(mongo_err);
+      throw new Error(Definer.auth_err1);
+    }
+  }
+
+  async getMemberArticlesData(member, mb_id, inquery) {
+    try {
+      mb_id = shapeIntoMongooseObjectId(mb_id);
+      const auth_mb_id = shapeIntoMongooseObjectId(member?._id),
+        page = inquery["page"] ? inquery["page"] * 1 : 1,
+        limit = inquery["limit"] ? inquery["limit"] * 1 : 5;
+      const result = await this.boArticleModel
+        .aggregate([
+          { $match: { mb_id: mb_id, art_status: "active" } },
+          { $sort: { createdAt: -1 } },
+          { $skip: (page - 1) * limit },
+          { $limit: limit },
+          {
+            $lookup: {
+              from: "members",
+              localField: "mb_id",
+              foreignField: "_id", 
+              as: "member_data", 
+            },
+          },
+          { $unwind: "$member_data" }, 
+        ])
+        .exec();
+      assert.ok(result, Definer.article_err2);
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+
+module.exports = Community;
